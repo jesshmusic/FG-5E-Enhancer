93803a98da4ae22046cc467be39de2a94c20ee81
jesshmusic <jhendricks@existentialmusic.com>
2021-05-22 11:16:01 -0400

    Adds token height stuff

--- scripts/token_wheel_manager.lua
+++ scripts/token_wheel_manager.lua
@@ -3,7 +3,6 @@
 ]]--
 
 function onInit()
-  Token.onWheelHelper = Token.onWheel;
   Token.onWheel = onWheel;
 end
 
@@ -13,12 +12,26 @@ function onWheel(token, notches)
       return;
   end
   local rotateLock = OptionsManager.getOption('CE_TRA');
-  if rotateLock == 'off' then
+
+    if Input.isShiftPressed() and User.isHost() then    
+        TokenHeight.updateHeight(token, notches);
+    elseif Input.isControlPressed() then
+        -- token scaling
+        Debug.console('5E Enhancer: Token.onWheel handler warning produced on purpose to force fallback to original token scaling code.');
+        Token.onWheel(token, notches);
+
+        --local scale = token.getContainerScale();
+        --scale = scale + notches * 0.1;
+        --token.setContainerScale(scale);
+
+        -- update health bar or dot graphics
+        --TokenManagerOverride.updateHealthHelper(token, CombatManager.getCTFromToken(token));
+    elseif rotateLock == 'off' then     
       -- token rotation for all
-    Token.onWheelHelper(token, notches);
+        token.setOrientation((token.getOrientation()+notches)%8);    
   elseif Input.isAltPressed() and rotateLock == 'on' then
       -- token rotation only when Alt pressed
-    Token.onWheelHelper(token, notches);
+        token.setOrientation((token.getOrientation()+notches)%8);
   end
 
   return true;
