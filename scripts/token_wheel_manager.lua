ee3d3f3d78424d2c8d9d31176829acd282e64dec
jesshmusic <jhendricks@existentialmusic.com>
2021-05-25 12:05:55 -0400

    fix: blood pool and token scaling

--- scripts/token_wheel_manager.lua
+++ scripts/token_wheel_manager.lua
@@ -3,7 +3,7 @@
 ]]--
 
 function onInit()
-  Token.onWheel = onWheel;
+  Token.onWheelHelper = onWheel;
 end
 
 
@@ -13,22 +13,14 @@ function onWheel(token, notches)
   end
   local rotateLock = OptionsManager.getOption('CE_TRA');
 
-    if Input.isShiftPressed() and User.isHost() then    
+    if Input.isShiftPressed() and User.isHost() then
         TokenHeight.updateHeight(token, notches);
     elseif Input.isControlPressed() then
         -- token scaling
-        Debug.console('5E Enhancer: Token.onWheel handler warning produced on purpose to force fallback to original token scaling code.');
-        Token.onWheel(token, notches);
-
-        --local scale = token.getContainerScale();
-        --scale = scale + notches * 0.1;
-        --token.setContainerScale(scale);
-
-        -- update health bar or dot graphics
-        --TokenManagerOverride.updateHealthHelper(token, CombatManager.getCTFromToken(token));
-    elseif rotateLock == 'off' then     
+        Token.onWheelHelper(token, notches);
+    elseif rotateLock == 'off' then
       -- token rotation for all
-        token.setOrientation((token.getOrientation()+notches)%8);    
+        token.setOrientation((token.getOrientation()+notches)%8);
   elseif Input.isAltPressed() and rotateLock == 'on' then
       -- token rotation only when Alt pressed
         token.setOrientation((token.getOrientation()+notches)%8);
