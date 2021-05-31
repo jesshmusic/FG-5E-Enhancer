2ec485c92de365e33a514d85af34f0c6431dad93
jesshmusic <jhendricks@existentialmusic.com>
2021-05-31 11:43:13 -0400

    fix: deletes some superfluous stuff

--- campaign/scripts/power_counter_override.lua
+++ campaign/scripts/power_counter_override.lua
@@ -1,8 +1,8 @@
--- only this function is overriden, rest are unchanged
+-- only this function is overridden, rest are unchanged
 function onClickDown(button, x, y)
 	-- update group visibility beforehand
 	window.parentcontrol.window.windowlist.window.onUsesChanged()
-	-- disable group visibility update 1 time 
+	-- disable group visibility update 1 time
 	Spell_Automation.SkipUsesUpdate = 1
 	return super.onClickDown(button, x, y)
 end
